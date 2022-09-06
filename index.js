09ae1c5d1def21d37f24ad182dcb085c6231bf2d
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2022-09-06 09:51:07 +0200

    Update `index.js`

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const cloudTwo = document.getElementById("cloud-two")
 const cloudThree = document.getElementById("cloud-three")
 const cloudFour = document.getElementById("cloud-four")
 const cloudFive = document.getElementById("cloud-five")
-const javascript = document.getElementById("javascript ")
+const javascript = document.getElementById("javascript")
 const react = document.getElementById("react")
-const graphql = document.getElementById("graphql ")
+const graphql = document.getElementById("graphql")
 
 function move() {
   const incrementer = window.scrollY
@@ -14,15 +14,18 @@ function move() {
   balloon.style.bottom = 10 + incrementer * 0.1 + "%"
 
   cloudOne.style.bottom = 40 + incrementer * 0.12 + "%"
-  cloudOne.style.marginLeft = 70 + incrementer * 0.1 + "%"
+  cloudOne.style.left = 75 + incrementer * 0.1 + "%"
   cloudTwo.style.bottom = 80 + incrementer * 0.14 + "%"
-  cloudTwo.style.marginLeft = 60 + incrementer * 0.15 + "%"
+  cloudTwo.style.left = 70 + incrementer * 0.15 + "%"
   cloudThree.style.bottom = 60 + incrementer * 0.1 + "%"
-  cloudThree.style.marginRight = 80 + incrementer * 0.12 + "%"
+  cloudThree.style.left = 0 + incrementer * -0.12 + "%"
   cloudFour.style.bottom = 70 + incrementer * 0.16 + "%"
-  cloudFour.style.marginRight = 40 + incrementer * 0.15 + "%"
+  cloudFour.style.left = 20 + incrementer * -0.15 + "%"
   cloudFive.style.bottom = 60 + incrementer * 0.2 + "%"
-  cloudFive.style.marginLeft = 40 + incrementer * 0.16 + "%"
+  cloudFive.style.left = 60 + incrementer * 0.16 + "%"
+  javascript.style.left = 20 + incrementer * -2.2 + "%"
+  react.style.left = 26 + incrementer * -1.5 + "%"
+  graphql.style.left = 32 + incrementer * -0.1 + "%"
 }
 
 window.addEventListener("scroll", move)
